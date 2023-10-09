facc89b6e51e7b1a45042570544caadca4a8746f
Jaiprakash Nagar <jaiprakashnagar07@gmail.com>
2023-10-09 20:39:25 +0530

    initialize npm file with package.json

---
+++ NpmModule/npm7.js
@@ -0,0 +1,15 @@
+// const ja = {
+//     name: 'jaiprakash',
+//     lname: 'nagar'
+// }
+// console.log(ja)
+
+// const pa = JSON.stringify(ja)
+// console.log(pa)
+// console.log(typeof pa)
+
+
+
+// const arr = [1, 2, 3]
+// console.log(arr, typeof arr)
+// console.log(JSON.stringify(arr), typeof JSON.stringify(arr))
